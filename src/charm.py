cb5594c08ef32dc2fa059368cd93ab340dc63c33
Amro Elamin <amro.elamin@outlook.com>
2026-06-03 14:25:02 -0400

    Updated charmcraft.yaml and charm.py with with metada and event handling code
    Signed-off-by: Amro Elamin <amro.elamin@outlook.com>

--- src/charm.py
+++ src/charm.py
@@ -1,82 +1,58 @@
 #!/usr/bin/env python3
-# Copyright 2026 root
-# See LICENSE file for licensing details.
 
-"""Charm the application."""
-
-import logging
-import time
+"""Kubernetes charm for a demo app."""
 
 import ops
+import logging
 
-# A standalone module for workload-specific logic (no charming concerns):
-import fastapi_demo
-
+# Log messages can be retrieved using juju debug-log
 logger = logging.getLogger(__name__)
 
-SERVICE_NAME = "some-service"  # Name of Pebble service that runs in the workload container.
 
+class FastAPIDemoCharm(ops.CharmBase):
+    """Charm the service."""
 
-class FastapiDemoCharm(ops.CharmBase):
-    """Charm the application."""
-
-    def __init__(self, framework: ops.Framework):
+    def __init__(self, framework: ops.Framework) -> None:
         super().__init__(framework)
-        framework.observe(self.on["some_container"].pebble_ready, self._on_pebble_ready)
-        self.container = self.unit.get_container("some-container")
+        framework.observe(self.on["demo-server"].pebble_ready, self._on_demo_server_pebble_ready)
+        self.pebble_service_name = "fastapi-service"
 
-    def _on_pebble_ready(self, event: ops.PebbleReadyEvent):
-        """Handle pebble-ready event."""
-        self.unit.status = ops.MaintenanceStatus("starting workload")
-        # To start the workload, we'll add a Pebble layer to the workload container.
-        # The layer specifies which service to run.
-        layer: ops.pebble.LayerDict = {
-            "services": {
-                SERVICE_NAME: {
-                    "override": "replace",
-                    "summary": "A service that runs in the workload container",
-                    "command": "/bin/foo",  # Change this!
-                    "startup": "enabled",
-                }
-            }
-        }
-        self.container.add_layer("base", layer, combine=True)
-        # If the container image is a rock, the container already has a Pebble layer.
-        # In this case, you could remove 'add_layer' or use 'add_layer' to extend the rock's layer.
-        # To learn about rocks, see https://documentation.ubuntu.com/rockcraft/en/stable/
-        self.container.replan()  # Starts the service (because 'startup' is enabled in the layer).
-        self.wait_for_ready()
-        version = fastapi_demo.get_version()
-        if version is not None:
-            self.unit.set_workload_version(version)
+    def _on_demo_server_pebble_ready(self, event: ops.PebbleReadyEvent) -> None:
+        """Define and start a workload using the Pebble API."""
+        # Get a reference the container attribute on the PebbleReadyEvent
+        container = event.workload
+        # Add initial Pebble config layer using the Pebble API
+        container.add_layer("fastapi_demo", self._get_pebble_layer(), combine=True)
+        # Make Pebble reevaluate its plan, ensuring any services are started if enabled.
+        container.replan()
+        # Learn more about statuses at
+        # https://documentation.ubuntu.com/juju/3.6/reference/status/
         self.unit.status = ops.ActiveStatus()
 
-    def is_ready(self) -> bool:
-        """Check whether the workload is ready to use."""
-        # We'll first check whether all Pebble services are running.
-        for name, service_info in self.container.get_services().items():
-            if not service_info.is_running():
-                logger.info("the workload is not ready (service '%s' is not running)", name)
-                return False
-        # The Pebble services are running, but the workload might not be ready to use.
-        # So we'll check whether all Pebble 'ready' checks are passing.
-        checks = self.container.get_checks(level=ops.pebble.CheckLevel.READY)
-        for check_info in checks.values():
-            if check_info.status != ops.pebble.CheckStatus.UP:
-                return False
-        return True
-
-    def wait_for_ready(self) -> None:
-        """Wait for the workload to be ready to use."""
-        for _ in range(3):
-            if self.is_ready():
-                return
-            time.sleep(1)
-        logger.error("the workload was not ready within the expected time")
-        raise RuntimeError("workload is not ready")
-        # The runtime error is for you (the charm author) to see, not for the user of the charm.
-        # Make sure that this function waits long enough for the workload to be ready.
+    def _get_pebble_layer(self) -> ops.pebble.Layer:
+        """Pebble layer for the FastAPI demo services."""
+        command = " ".join(
+            [
+                "uvicorn",
+                "api_demo_server.app:app",
+                "--host=0.0.0.0",
+                "--port=8000",
+            ]
+        )
+        pebble_layer: ops.pebble.LayerDict = {
+            "summary": "FastAPI demo service",
+            "description": "pebble config layer for FastAPI demo server",
+            "services": {
+                self.pebble_service_name: {
+                    "override": "replace",
+                    "summary": "fastapi demo",
+                    "command": command,
+                    "startup": "enabled",
+                }
+            },
+        }
+        return ops.pebble.Layer(pebble_layer)
 
 
 if __name__ == "__main__":  # pragma: nocover
-    ops.main(FastapiDemoCharm)
+    ops.main(FastAPIDemoCharm)
