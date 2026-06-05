96ffbda42bf2954c2a0247a795cdd6d75a54e8bc
Amro Elamin <amro.elamin@outlook.com>
2026-06-05 11:35:01 -0400

    test: Add unit and integration tests
    Signed-off-by: Amro Elamin <amro.elamin@outlook.com>

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -1,95 +1,37 @@
-# Copyright 2026 root
-# See LICENSE file for licensing details.
-#
-# To learn more about testing, see https://documentation.ubuntu.com/ops/latest/explanation/testing/
-
 import ops
-import pytest
 from ops import testing
 
-from charm import SERVICE_NAME, FastapiDemoCharm
+from charm import FastAPIDemoCharm
 
-CHECK_NAME = "service-ready"  # Name of Pebble check in the mock workload container.
 
-# A minimal Pebble layer for our testing.Container objects.
-# Our charm doesn't retrieve the service command or the check URL
-# from Pebble, so this layer doesn't need a real command or URL.
-MOCK_LAYER = ops.pebble.Layer(
-    {
+def test_pebble_layer():
+    ctx = testing.Context(FastAPIDemoCharm)
+    container = testing.Container(name="demo-server", can_connect=True)
+    state_in = testing.State(
+        containers={container},
+        leader=True,
+    )
+    state_out = ctx.run(ctx.on.pebble_ready(container), state_in)
+    # Expected plan after Pebble ready with default config
+    expected_plan = {
         "services": {
-            SERVICE_NAME: {
+            "fastapi-service": {
                 "override": "replace",
-                "command": "mock-command",
+                "summary": "fastapi demo",
+                "command": "uvicorn api_demo_server.app:app --host=0.0.0.0 --port=8000",
                 "startup": "enabled",
+                # Since the environment is empty, Layer.to_dict() will not
+                # include it.
             }
-        },
-        "checks": {
-            CHECK_NAME: {
-                "override": "replace",
-                "level": "ready",
-                "threshold": 3,
-                "startup": "enabled",
-                "http": {
-                    "url": "http://localhost:1234/mock-endpoint",
-                },
-            }
-        },
+        }
     }
-)
 
-
-def mock_get_version():
-    """Get a mock version string without executing the workload code."""
-    return "1.0.0"
-
-
-def test_pebble_ready(monkeypatch: pytest.MonkeyPatch):
-    """Test that the charm has the correct state after handling the pebble-ready event."""
-    # Arrange:
-    ctx = testing.Context(FastapiDemoCharm)
-    check_in = testing.CheckInfo(
-        CHECK_NAME,
-        level=ops.pebble.CheckLevel.READY,
-        status=ops.pebble.CheckStatus.UP,  # Simulate the Pebble check passing.
-    )
-    container_in = testing.Container(
-        "some-container",
-        can_connect=True,
-        layers={"base": MOCK_LAYER},
-        service_statuses={SERVICE_NAME: ops.pebble.ServiceStatus.INACTIVE},
-        check_infos={check_in},
-    )
-    state_in = testing.State(containers={container_in})
-    monkeypatch.setattr("charm.fastapi_demo.get_version", mock_get_version)
-
-    # Act:
-    state_out = ctx.run(ctx.on.pebble_ready(container_in), state_in)
-
-    # Assert:
-    container_out = state_out.get_container(container_in.name)
-    assert container_out.service_statuses[SERVICE_NAME] == ops.pebble.ServiceStatus.ACTIVE
-    assert state_out.workload_version is not None
+    # Check that we have the plan we expected:
+    assert state_out.get_container(container.name).plan == expected_plan
+    # Check the unit is active:
     assert state_out.unit_status == testing.ActiveStatus()
-
-
-def test_pebble_ready_service_not_ready():
-    """Test that the charm raises an error if the workload isn't ready after Pebble starts it."""
-    # Arrange:
-    ctx = testing.Context(FastapiDemoCharm)
-    check_in = testing.CheckInfo(
-        CHECK_NAME,
-        level=ops.pebble.CheckLevel.READY,
-        status=ops.pebble.CheckStatus.DOWN,  # Simulate the Pebble check failing.
+    # Check the service was started:
+    assert (
+        state_out.get_container(container.name).service_statuses["fastapi-service"]
+        == ops.pebble.ServiceStatus.ACTIVE
     )
-    container_in = testing.Container(
-        "some-container",
-        can_connect=True,
-        layers={"base": MOCK_LAYER},
-        service_statuses={SERVICE_NAME: ops.pebble.ServiceStatus.INACTIVE},
-        check_infos={check_in},
-    )
-    state_in = testing.State(containers={container_in})
-
-    # Act & assert:
-    with pytest.raises(testing.errors.UncaughtCharmError):
-        ctx.run(ctx.on.pebble_ready(container_in), state_in)
