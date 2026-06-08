65285fe42ef3128bbf4e2da015c15a95b547da2a
Amro Elamin <amro.elamin@outlook.com>
2026-06-07 23:34:59 -0400

    test: add integration tests for database integration
    Signed-off-by: Amro Elamin <amro.elamin@outlook.com>

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -7,15 +7,30 @@ import yaml
 
 logger = logging.getLogger(__name__)
 
-METADATA = yaml.safe_load(pathlib.Path("charmcraft.yaml").read_text())
+METADATA = yaml.safe_load(pathlib.Path("./charmcraft.yaml").read_text())
 APP_NAME = METADATA["name"]
 
 
 @pytest.mark.juju_setup
 def test_deploy(charm: pathlib.Path, juju: jubilant.Juju):
-    """Deploy the charm under test."""
+    """Deploy the charm under test.
+
+    Assert on the unit status before any relations/configurations take place.
+    """
     resources = {
         "demo-server-image": METADATA["resources"]["demo-server-image"]["upstream-source"]
     }
+
+    # Deploy the charm and wait for it to report blocked, as it needs Postgres.
     juju.deploy(charm, app=APP_NAME, resources=resources)
+    juju.wait(jubilant.all_blocked)
+
+@pytest.mark.juju_setup
+def test_database_integration(charm: pathlib.Path, juju: jubilant.Juju):
+    """Verify that the charm integrates with the database.
+
+    Assert that the charm is active if the integration is established.
+    """
+    juju.deploy("postgresql-k8s", channel="14/stable", trust=True)
+    juju.integrate(APP_NAME, "postgresql-k8s")
     juju.wait(jubilant.all_active)
